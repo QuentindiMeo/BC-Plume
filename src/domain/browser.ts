ad698caf2f64c8a81fb386397bfc1fcb146cd580
Quentin DI MEO <q.dimeo@outlook.com>
2026-05-10 22:04:24 +0200

    BPE26-077: add donation cache keys, donation toast locale keys

--- src/domain/browser.ts
+++ src/domain/browser.ts
@@ -20,6 +20,8 @@ export enum PLUME_CACHE_KEYS {
 
   // Meta
   LAST_SEEN_RELEASE = "plume_last_seen_release",
+  FULL_PLAY_COUNT = "plume_full_play_count",
+  DONATION_DISMISSAL_COUNT = "plume_donation_dismissal_count",
 }
 
 export interface LocalStorage {
@@ -36,5 +38,7 @@ export interface LocalStorage {
   [PLUME_CACHE_KEYS.FEATURE_FLAGS]: FeatureFlags | undefined;
 
   [PLUME_CACHE_KEYS.LAST_SEEN_RELEASE]: string | undefined;
+  [PLUME_CACHE_KEYS.FULL_PLAY_COUNT]: number | undefined;
+  [PLUME_CACHE_KEYS.DONATION_DISMISSAL_COUNT]: number | undefined;
 }
 export type PlumeCacheKey = keyof LocalStorage;
